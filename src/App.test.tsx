6de1b13c969a1b03415834879a3fb1af75b21631
Arjun Reddy <mail.arjunreddy001@gmail.com>
2021-06-15 15:26:54 +0530

    Cleanup of unwanted files & code, Sass Installed

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component", () => {
+  test("renders 'Portfolio Site' text", () => {
+    render(<App />);
+    const headingElement = screen.getByText(/Portfolio Site/i);
+    expect(headingElement).toBeInTheDocument();
+  });
 });
