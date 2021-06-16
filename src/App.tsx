683761d09ceb9a89431edc66a30019dd12b631ec
Arjun Reddy <mail.arjunreddy001@gmail.com>
2021-06-16 18:24:08 +0530

    feat: navigation (router) implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
+import React, { Suspense } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 import "./App.scss";
+import Layout from "./components/layout/Layout";
+
+const HomePage = React.lazy(() => import("./pages/HomePage"));
+const AboutPage = React.lazy(() => import("./pages/AboutPage"));
+const ProjectsPage = React.lazy(() => import("./pages/ProjectsPage"));
+const ContactPage = React.lazy(() => import("./pages/ContactPage"));
+const NotFoundPage = React.lazy(() => import("./pages/404"));
 
 function App() {
   return (
     <div>
-      <h1>Portfolio Site</h1>
+      <Layout>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Switch>
+            <Route path="/" exact>
+              <HomePage />
+            </Route>
+            <Route path="/home" exact>
+              <Redirect to="/" />
+            </Route>
+            <Route path="/about" exact>
+              <AboutPage />
+            </Route>
+            <Route path="/projects" exact>
+              <ProjectsPage />
+            </Route>
+            <Route path="/contact" exact>
+              <ContactPage />
+            </Route>
+            <Route path="*">
+              <NotFoundPage />
+            </Route>
+          </Switch>
+        </Suspense>
+      </Layout>
     </div>
   );
 }
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,4 +1,5 @@
 import styles from "./Navbar.module.scss";
+import { NavLink } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   return (
@@ -7,10 +8,34 @@ const Navbar: React.FC = () => {
         <div className={styles["navbar-logo"]}>ARJUN REDDY</div>
         <div className={styles["navs"]}>
           <ul className={styles["nav-links"]}>
-            <li>Home</li>
-            <li>About</li>
-            <li>Projects</li>
-            <li>Contact</li>
+            <li>
+              <NavLink to="/" exact activeClassName={styles["nav-active"]}>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" exact activeClassName={styles["nav-active"]}>
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/projects"
+                exact
+                activeClassName={styles["nav-active"]}
+              >
+                Projects
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/contact"
+                exact
+                activeClassName={styles["nav-active"]}
+              >
+                Contact
+              </NavLink>
+            </li>
           </ul>
           <button type="button" className={styles["btn-download"]}>
             Download CV
