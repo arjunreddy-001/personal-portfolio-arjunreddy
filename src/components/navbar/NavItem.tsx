b06cbb9e004524d5540a145afe6133c586508da7
Arjun Reddy <mail.arjunreddy001@gmail.com>
2021-06-19 23:36:26 +0530

    feat: responsive navbar

--- src/components/navbar/NavItem.tsx
+++ src/components/navbar/NavItem.tsx
@@ -2,10 +2,22 @@ import React from "react";
 import styles from "./NavItem.module.scss";
 import { NavLink } from "react-router-dom";
 
-const NavItem: React.FC<{ to: string; text: string }> = ({ to, text }) => {
+const NavItem: React.FC<{
+  to: string;
+  text: string;
+  icon: React.ReactElement;
+  onNavLinkClick: () => void;
+}> = ({ to, text, icon, onNavLinkClick }) => {
   return (
     <li className={styles["nav-item"]}>
-      <NavLink to={to} exact activeClassName={styles["nav-active"]}>
+      <NavLink
+        to={to}
+        exact
+        className={styles["nav-link"]}
+        activeClassName={styles["nav-active"]}
+        onClick={onNavLinkClick}
+      >
+        <span className={styles["nav-icon"]}>{icon}</span>
         {text}
       </NavLink>
     </li>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,29 +1,90 @@
+import { useState } from "react";
 import styles from "./Navbar.module.scss";
 import NavItem from "./NavItem";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import {
+  faDownload,
+  faHome,
+  faUser,
+  faTimes,
+  faBars,
+  faCommentDots,
+  faImage,
+} from "@fortawesome/free-solid-svg-icons";
 
 const downloadIcon = <FontAwesomeIcon icon={faDownload} />;
+const homeIcon = <FontAwesomeIcon icon={faHome} />;
+const aboutIcon = <FontAwesomeIcon icon={faUser} />;
+const portfolioIcon = <FontAwesomeIcon icon={faImage} />;
+const contactIcon = <FontAwesomeIcon icon={faCommentDots} />;
+const closeIcon = (
+  <FontAwesomeIcon icon={faTimes} className={styles["nav-close"]} />
+);
+const toggleNavIcon = <FontAwesomeIcon icon={faBars} />;
 
 const Navbar: React.FC = () => {
+  const [isNavShown, setIsNavShown] = useState(false);
+
+  const showNav = () => {
+    setIsNavShown(true);
+  };
+  const closeNav = () => {
+    setIsNavShown(false);
+  };
+
+  const navsClasses = `${styles["navs"]} ${
+    isNavShown ? styles["show-nav"] : ""
+  }`;
+
   return (
-    <nav className={styles["navbar"]}>
-      <div className={styles["navbar-content-wrapper"]}>
+    <header className={styles["navbar"]}>
+      <nav className={styles["navbar-content-wrapper"]}>
         <div className={styles["navbar-logo"]}>ARJUN REDDY</div>
-        <div className={styles["navs"]}>
+        <div className={navsClasses}>
           <ul className={styles["nav"]}>
-            <NavItem to="/" text="Home" />
-            <NavItem to="/about" text="About" />
-            <NavItem to="/projects" text="Projects" />
-            <NavItem to="/contact" text="Contact" />
+            <NavItem
+              to="/"
+              text="Home"
+              icon={homeIcon}
+              onNavLinkClick={closeNav}
+            />
+            <NavItem
+              to="/about"
+              text="About"
+              icon={aboutIcon}
+              onNavLinkClick={closeNav}
+            />
+            <NavItem
+              to="/projects"
+              text="Projects"
+              icon={portfolioIcon}
+              onNavLinkClick={closeNav}
+            />
+            <NavItem
+              to="/contact"
+              text="Contact"
+              icon={contactIcon}
+              onNavLinkClick={closeNav}
+            />
+            <div className={styles["nav-close"]} onClick={closeNav}>
+              {closeIcon}
+            </div>
           </ul>
+          <div className={styles["btn-container"]}>
+            <button type="button" className={styles["btn-download"]}>
+              <span className={styles["btn-text"]}>Download CV</span>
+              <span className={styles["btn-icon"]}>{downloadIcon}</span>
+            </button>
+          </div>
         </div>
-        <button type="button" className={styles["btn-download"]}>
-          <span className={styles["btn-text"]}>Download CV</span>
-          <span className={styles["btn-icon"]}>{downloadIcon}</span>
-        </button>
-      </div>
-    </nav>
+
+        <div className={styles["hamburger"]}>
+          <div className={styles["nav-toggle"]} onClick={showNav}>
+            {toggleNavIcon}
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 };
 
