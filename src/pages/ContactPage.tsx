d8220a62093d9dea0aee0a252bc8d363679a8b0b
Arjun Reddy <mail.arjunreddy001@gmail.com>
2021-06-24 16:56:47 +0530

    feat: contact form can send message

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,27 +1,45 @@
 import { useRef } from "react";
 import styles from "./ContactPage.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faAngleDoubleRight} from "@fortawesome/free-solid-svg-icons";
+import { faAngleDoubleRight } from "@fortawesome/free-solid-svg-icons";
 import { FormEvent } from "react";
 
 const rightArrowIcon = <FontAwesomeIcon icon={faAngleDoubleRight} />;
 
 const ContactPage: React.FC = () => {
-
   const nameRef = useRef<HTMLInputElement>(null);
   const emailRef = useRef<HTMLInputElement>(null);
   const messageRef = useRef<HTMLTextAreaElement>(null);
 
   const sendMessageHandler = (event: FormEvent) => {
     event.preventDefault();
-    console.log(nameRef.current?.value);
-    console.log(emailRef.current?.value);
-    console.log(messageRef.current?.value);
 
+    const url = "https://formsubmit.co/ajax/b7c8a12cd083761a2f44481ad4834437";
+
+    const data = {
+      name: nameRef.current!.value,
+      email: emailRef.current!.value,
+      message: messageRef.current!.value,
+      _subject: nameRef.current!.value + " via Contact Form - arjunreddy.in",
+      _template: "table",
+      _replyto: emailRef.current!.value,
+    };
+
+    // Send mail using mail forwarder service (formsubmit.co)
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    // Clear input fields of contact form
     nameRef.current!.value = "";
     emailRef.current!.value = "";
     messageRef.current!.value = "";
-  }
+  };
 
   return (
     <section className={styles["contact-section"]}>
@@ -31,19 +49,30 @@ const ContactPage: React.FC = () => {
 
         <form onSubmit={sendMessageHandler} className={styles["contact-form"]}>
           <div className={styles["contact-inputs"]}>
-
             <div className={styles["contact-content"]}>
               <label htmlFor="name" className={styles["contact-label"]}>
                 Name
               </label>
-              <input type="text" ref={nameRef} id="name" className={styles["contact-input"]} required/>
+              <input
+                type="text"
+                ref={nameRef}
+                id="name"
+                className={styles["contact-input"]}
+                required
+              />
             </div>
 
             <div className={styles["contact-content"]}>
               <label htmlFor="email" className={styles["contact-label"]}>
                 Email
               </label>
-              <input type="email" ref={emailRef} id="email" className={styles["contact-input"]} required/>
+              <input
+                type="email"
+                ref={emailRef}
+                id="email"
+                className={styles["contact-input"]}
+                required
+              />
             </div>
 
             <div className={styles["contact-content"]}>
